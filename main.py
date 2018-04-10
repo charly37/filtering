776a03a578e594613b1675f7fed90979060524d8
Charles Walker <charles.walker.37@gmail.com>
2018-04-10 19:00:55 -0400

    Doc

--- main.py
+++ main.py
@@ -10,25 +10,33 @@ def filtering(aPostContent):
     aPhoneNumberMatch=reg.search(aPostContent)
     if(aPhoneNumberMatch):
         print("This post need to be filter due to phone number: ", aPhoneNumberMatch.group())
-        return True
+        return 1
     reg2 = re.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
     aEmailMatch=reg2.search(aPostContent)
     if(aEmailMatch):
         print("This post need to be filter due to email: ", aEmailMatch.group())
-        return True
-    return False
+        return 2
+    return 0
 
 with open("waround.csv", encoding="utf8") as f:
     #CSV header: Anonymous Link,ww_uuid,post_uuid,content,Word Count
     reader = csv.DictReader(f)
     aNbImage=0
+    aNbEmailPost=0
+    aNbPhoneNumberPost=0
     for aOneEntry in reader:
         #print("Working on: ", aOneEntry)
         #print("Content to clean: ", aOneEntry["content"])
         if(aOneEntry["content"] == "image post"):
             aNbImage = aNbImage + 1
-        if(filtering(aOneEntry["content"])):
-            print("Please clean the post with ID: ", aOneEntry["post_uuid"] ," and content: ", aOneEntry["content"])
+        aRetCode=filtering(aOneEntry["content"])
+        if(aRetCode>0):
+            print("Please clean the post with ID: ", aOneEntry["post_uuid"] ," and content: ", aOneEntry["content"], "\n")
+            if(aRetCode==1):
+                aNbPhoneNumberPost=aNbPhoneNumberPost+1
+            if(aRetCode==2):
+                aNbEmailPost=aNbEmailPost+1
+    print("aNbPhoneNumberPost: ",str(aNbPhoneNumberPost)," aNbEmailPost: ",str(aNbEmailPost))
     #print("For info there are ", str(aNbImage), " post with only image")
 
 
