71fa410583cb457102bfd0dd0a81d32f731be48c
Charles WALKER <charles.walker@amadeus.com>
2018-04-11 10:46:32 -0400

    Simplify and try to add some args to do replace instead of flag

--- main.py
+++ main.py
@@ -1,45 +1,48 @@
 import csv
 import re
+import argparse
+import time
+
+#https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-python-programs-execution
+start_time = time.time()
 
 
 print("Starting - The file to read need to be in the same folder and called waround.csv")
 
-def filtering(aPostContent):
+parser = argparse.ArgumentParser(description='Look for some email address or phone number in csv file.')
+parser.add_argument('--filter', action='store_true')
+args = parser.parse_args()
+
+
+def filtering(aPost):
     #https://stackoverflow.com/questions/3868753/find-phone-numbers-in-python-script
-    reg = re.compile("\d{3}\d{3}\d{4}")
-    aPhoneNumberMatch=reg.search(aPostContent)
+    aPhoneNumberRegex = re.compile(r"\d{3}\d{3}\d{4}")
+    aPhoneNumberMatch=aPhoneNumberRegex.search(aPost["content"])
     if(aPhoneNumberMatch):
-        print("This post need to be filter due to phone number: ", aPhoneNumberMatch.group())
-        return 1
+        print("This post ID", aPost["post_uuid"], "need to be filter due to phone number:", aPhoneNumberMatch.group(), "with post content:", aPost["content"])
     #https://stackoverflow.com/questions/17681670/extract-email-sub-strings-from-large-document
-    reg2 = re.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
-    aEmailMatch=reg2.search(aPostContent)
+    aEmailAdressRegex = re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
+    aEmailMatch=aEmailAdressRegex.search(aPost["content"])
     if(aEmailMatch):
-        print("This post need to be filter due to email: ", aEmailMatch.group())
-        return 2
-    return 0
+        print("This post ID", aPost["post_uuid"], "need to be filter due to email:", aEmailMatch.group(), "with post content:", aPost["content"])
+    
+
+
+if (args.filter):
+    print("Warning i will filter")
 
 with open("waround.csv", encoding="utf8") as f:
     #CSV header: Anonymous Link,ww_uuid,post_uuid,content,Word Count
     reader = csv.DictReader(f)
-    aNbImage=0
     aNbEmailPost=0
     aNbPhoneNumberPost=0
-    for aOneEntry in reader:
-        #print("Working on: ", aOneEntry)
-        #print("Content to clean: ", aOneEntry["content"])
-        if(aOneEntry["content"] == "image post"):
-            aNbImage = aNbImage + 1
-        aRetCode=filtering(aOneEntry["content"])
-        if(aRetCode>0):
-            print("Please clean the post with ID: ", aOneEntry["post_uuid"] ," and content: ", aOneEntry["content"], "\n")
-            if(aRetCode==1):
-                aNbPhoneNumberPost=aNbPhoneNumberPost+1
-            if(aRetCode==2):
-                aNbEmailPost=aNbEmailPost+1
-    print("aNbPhoneNumberPost: ",str(aNbPhoneNumberPost)," aNbEmailPost: ",str(aNbEmailPost))
-    #print("For info there are ", str(aNbImage), " post with only image")
 
+    #Also open a file to write data
+    with open("testfile.txt","w") as filteredFile:
 
+        for aOneEntry in reader:
+            #print("Working on: ", aOneEntry)
+            #print("Content to clean: ", aOneEntry["content"])
+            filtering(aOneEntry)
 
-print("Ending")
+print("Ending in ", (time.time() - start_time),  " ms")
