11b90dc78eadc4de7138fe0433359b5393586469
Charles Walker <charles.walker.37@gmail.com>
2018-04-11 22:16:23 -0400

    Smarter

--- main.py
+++ main.py
@@ -14,21 +14,24 @@ parser.add_argument('--filter', action='store_true')
 args = parser.parse_args()
 
 
-def filtering(aPost):
+def filtering(aPost,iMatchInfo):
     #https://stackoverflow.com/questions/3868753/find-phone-numbers-in-python-script
     aPhoneNumberRegex = re.compile(r"\d{3}\d{3}\d{4}")
     aPhoneNumberMatch=aPhoneNumberRegex.search(aPost["content"])
     if(aPhoneNumberMatch):
-        print("This post ID", aPost["post_uuid"], "need to be filter due to phone number:", aPhoneNumberMatch.group(), "with post content:", aPost["content"])
+        print("This post ID", aPost["post_uuid"], "need to be filter due to phone number:", aPhoneNumberMatch.group())
+        #print("This post ID", aPost["post_uuid"], "need to be filter due to phone number:", aPhoneNumberMatch.group(), "with post content:", aPost["content"])
+        iMatchInfo[0]=iMatchInfo[0]+1
         if (args.filter):
-            aPost["content"]=re.sub(r"\d{3}\d{3}\d{4}","**********",aPost["content"])
+            aPost["content"]=re.sub(r"(\d{3}\d{3}\d{4})",r'___WARNINGPHONE:\1___',aPost["content"])
     #https://stackoverflow.com/questions/17681670/extract-email-sub-strings-from-large-document
     aEmailAdressRegex = re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
     aEmailMatch=aEmailAdressRegex.search(aPost["content"])
     if(aEmailMatch):
-        print("This post ID", aPost["post_uuid"], "need to be filter due to email:", aEmailMatch.group(), "with post content:", aPost["content"])
+        #print("This post ID", aPost["post_uuid"], "need to be filter due to email:", aEmailMatch.group(), "with post content:", aPost["content"])
+        iMatchInfo[1]=iMatchInfo[1]+1
         if (args.filter):
-            aPost["content"]=re.sub(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+","**********",aPost["content"])
+            aPost["content"]=re.sub(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",r"___WARNINGEMAILADD:\1___",aPost["content"])
     return aPost
     
 
@@ -41,17 +44,20 @@ with open("waround.csv", encoding="utf8") as f:
     reader = csv.DictReader(f)
     aNbEmailPost=0
     aNbPhoneNumberPost=0
+    #number of phone and email
+    aMatchInfo=[0,0]
 
     #Also open a file to write data
     with open("output.csv","w", encoding="utf8",newline='') as filteredFile:
-        fieldnames = ['Anonymous Link', 'ww_uuid','post_uuid','content','Word Count']
+        fieldnames = ['linenumber', 'post_uuid','content']
         writer = csv.DictWriter(filteredFile, fieldnames=fieldnames)
         writer.writeheader()
 
         for aOneEntry in reader:
             #print("Working on: ", aOneEntry)
             #print("Content to clean: ", aOneEntry["content"])
-            filtering(aOneEntry)
+            filtering(aOneEntry,aMatchInfo)
             writer.writerow(aOneEntry)
+    print("info: ", str(aMatchInfo))
 
 print("Ending in ", (time.time() - start_time),  " ms")
