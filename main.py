37bd8fff64fe0ea5e496f7d29c82a9ddf7afc394
Charles Walker <charles.walker.37@gmail.com>
2018-04-16 18:23:05 -0400

    More generic scripts which remove hardcode for header row and able to process list of files

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ import time
 #https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-python-programs-execution
 start_time = time.time()
 
-
-print("Starting - The file to read need to be in the same folder and called waround.csv")
-
 parser = argparse.ArgumentParser(description='Look for some email address or phone number in csv file.')
 parser.add_argument('--filter', action='store_true')
+
+# https://stackoverflow.com/questions/15753701/argparse-option-for-passing-a-list-as-option
+# This is the correct way to handle accepting multiple arguments.
+# '+' == 1 or more.
+# '*' == 0 or more.
+# '?' == 0 or 1.
+# An int is an explicit number of arguments to accept.
+parser.add_argument('--filelist', nargs='+')
+
+
 args = parser.parse_args()
 
 
@@ -33,31 +40,39 @@ def filtering(aPost,iMatchInfo):
         if (args.filter):
             aPost["content"]=re.sub(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",r"___WARNINGEMAILADD:\1___",aPost["content"])
     return aPost
-    
+
+def filterFile(aFilename):
+    with open(aFilename, encoding="utf8") as f:
+        #CSV header: Anonymous Link,ww_uuid,post_uuid,content,Word Count
+        reader = csv.DictReader(f)
+        aNbEmailPost=0
+        aNbPhoneNumberPost=0
+        #number of phone and email
+        aMatchInfo=[0,0]
+
+        #Also open a file to write data
+        with open(aFilename + "_output.csv","w", encoding="utf8",newline='') as filteredFile:
+            fieldnames = reader.fieldnames
+            writer = csv.DictWriter(filteredFile, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for aOneEntry in reader:
+                #print("Working on: ", aOneEntry)
+                #print("Content to clean: ", aOneEntry["content"])
+                filtering(aOneEntry,aMatchInfo)
+                writer.writerow(aOneEntry)
+        print("info: ", str(aMatchInfo))
+
+print("Ending in ", (time.time() - start_time),  " ms")
 
 
 if (args.filter):
     print("Warning i will filter")
 
-with open("waround.csv", encoding="utf8") as f:
-    #CSV header: Anonymous Link,ww_uuid,post_uuid,content,Word Count
-    reader = csv.DictReader(f)
-    aNbEmailPost=0
-    aNbPhoneNumberPost=0
-    #number of phone and email
-    aMatchInfo=[0,0]
+print("Going to work on files:", str(args.filelist))
+for aOneFile in args.filelist:
+    print("Current file:", aOneFile)
+    filterFile(aOneFile)
 
-    #Also open a file to write data
-    with open("output.csv","w", encoding="utf8",newline='') as filteredFile:
-        fieldnames = ['linenumber', 'post_uuid','content']
-        writer = csv.DictWriter(filteredFile, fieldnames=fieldnames)
-        writer.writeheader()
 
-        for aOneEntry in reader:
-            #print("Working on: ", aOneEntry)
-            #print("Content to clean: ", aOneEntry["content"])
-            filtering(aOneEntry,aMatchInfo)
-            writer.writerow(aOneEntry)
-    print("info: ", str(aMatchInfo))
 
-print("Ending in ", (time.time() - start_time),  " ms")
