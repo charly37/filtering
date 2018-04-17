8ec3d741717300f886263e161fdee1a96cbbf7cc
Charles Walker <charles.walker.37@gmail.com>
2018-04-16 23:17:03 -0400

    more filter

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def filtering(aPost,iMatchInfo):
         #print("This post ID", aPost["post_uuid"], "need to be filter due to phone number:", aPhoneNumberMatch.group(), "with post content:", aPost["content"])
         iMatchInfo[0]=iMatchInfo[0]+1
         if (args.filter):
-            aPost["content"]=re.sub(r"(\d{3}\d{3}\d{4})",r'___WARNINGPHONE:\1___',aPost["content"])
+            aPost["content"]=re.sub(r"(\d{3}\d{3}\d{4})",r'___WARNINGPHONE___',aPost["content"])
     #https://stackoverflow.com/questions/17681670/extract-email-sub-strings-from-large-document
     aEmailAdressRegex = re.compile(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
     aEmailMatch=aEmailAdressRegex.search(aPost["content"])
@@ -59,14 +59,41 @@ def filtering(aPost,iMatchInfo):
         #logger.info("This post ID " + str(aPost["post_uuid"]) + " need to be filter due to email: " + str(aEmailMatch.group()))
         iMatchInfo[1]=iMatchInfo[1]+1
         if (args.filter):
-            aPost["content"]=re.sub(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",r"___WARNINGEMAILADD:\1___",aPost["content"])
+            aPost["content"]=re.sub(r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)",r"___WARNINGEMAILADD___",aPost["content"])
     return aPost
 
 def tagging(aPost):
-    aRsvpRegex = re.compile(r"rsvp")
+    aTags=[]
+    aRsvpRegex = re.compile(r"rsvp|come visit|come join|Head down|Orientation|Come on down|Head up to|Join us|Visit us|Lunch & learn|Workshop|Event|Gala|party")
     aRsvpMatch=aRsvpRegex.search(aPost["content"],re.IGNORECASE)
     if(aRsvpMatch):
         logger.info("This post ID " + aPost["post_uuid"] +  " may be a RESA because of matching keyword: " + str(aRsvpMatch.group())+ " with full post content: " + str(aPost["content"]))
+        aTags.append("RSVP")
+
+    aRequestHelpRegex = re.compile(r"hiring|how do I|anyone good at|can anyone recommend|any recommendations|what’s the best way|referral|refer")
+    aRequestHelpMatch=aRequestHelpRegex.search(aPost["content"],re.IGNORECASE)
+    if(aRequestHelpMatch):
+        logger.info("This post ID " + aPost["post_uuid"] +  " may be a Help Request because of matching keyword: " + str(aRequestHelpMatch.group())+ " with full post content: " + str(aPost["content"]))
+        aTags.append("HELP")
+
+    aOtherRegex = re.compile(r"did anyone find|lost")
+    aOtherMatch=aOtherRegex.search(aPost["content"],re.IGNORECASE)
+    if(aOtherMatch):
+        logger.info("This post ID " + aPost["post_uuid"] +  " may be a Other because of matching keyword: " + str(aOtherMatch.group())+ " with full post content: " + str(aPost["content"]))
+        aTags.append("OTHER")
+
+    aExchangeItemRegex = re.compile(r"charger|cable|borrow|lend me|loan me|for rent|trade me|trade for")
+    aExchangeItemMatch=aExchangeItemRegex.search(aPost["content"],re.IGNORECASE)
+    if(aExchangeItemMatch):
+        logger.info("This post ID " + aPost["post_uuid"] +  " may be a Exchange Item because of matching keyword: " + str(aExchangeItemMatch.group())+ " with full post content: " + str(aPost["content"]))
+        aTags.append("EXCHANGE")
+
+    aPromoteRegex = re.compile(r"Discount|%")
+    aPromoteMatch=aPromoteRegex.search(aPost["content"],re.IGNORECASE)
+    if(aPromoteMatch):
+        logger.info("This post ID " + aPost["post_uuid"] +  " may be a Promote because of matching keyword: " + str(aPromoteMatch.group())+ " with full post content: " + str(aPost["content"]))
+        aTags.append("PROMO")
+    return aTags
 
 def filterFile(aFilename):
     with open(aFilename, encoding="utf8") as f:
@@ -79,7 +106,7 @@ def filterFile(aFilename):
 
         #Also open a file to write data
         with open(aFilename + "_output.csv","w", encoding="utf8",newline='') as filteredFile:
-            fieldnames = reader.fieldnames
+            fieldnames = reader.fieldnames + ["TAGS"]
             writer = csv.DictWriter(filteredFile, fieldnames=fieldnames)
             writer.writeheader()
 
@@ -87,8 +114,10 @@ def filterFile(aFilename):
                 #print("Working on: ", aOneEntry)
                 #print("Content to clean: ", aOneEntry["content"])
                 filtering(aOneEntry,aMatchInfo)
+                aTags = tagging(aOneEntry)
+                aOneEntry["TAGS"]= '-'.join(aTags)
                 writer.writerow(aOneEntry)
-                tagging(aOneEntry)
+                
         logger.info("We filter " + str(aMatchInfo[0]) + " phone numbers and " + str(aMatchInfo[1]) + " emails.")
 
 logger.info("Ending in " + str((time.time() - start_time)) + " ms")
